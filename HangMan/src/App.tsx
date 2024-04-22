4a15b19f2b248dd75d671507afd8a5a328f48181
SamsonPG <121846435+SamsonPG@users.noreply.github.com>
2024-04-22 20:12:13 +0530

    updated functions

--- HangMan/src/App.tsx
+++ HangMan/src/App.tsx
@@ -1,17 +1,34 @@
 import { useState } from "react";
-import words from "./wordList.json"
-
-function App(){
-
-  const [wordToGuess,setWordToGuess] = useState(()=>{
-    return words[Math.floor(Math.random()* words.length)]
+import words from "./wordList.json";
+import { HangmanDrawing } from "./components/HangmanDrawing";
+import { Keyboard } from "./components/Keyboard";
+import { HangmanWord } from "./components/HangmanWord";
+function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
   });
 
-  const [gussedLetters,setGussedLetters] = useState<string[]>([])
+  const [gussedLetters, setGussedLetters] = useState<string[]>([]);
 
-console.log(wordToGuess);
+  console.log(wordToGuess);
 
-  return (<h1>Hi</h1>)
+  return (
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>Lose Win</div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+    </div>
+  );
 }
 
-export default App
+export default App;
